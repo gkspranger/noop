1557c3a738d774ccf81ccca8928bebef76ede93d
gkspranger <greg@spranger.us>
2022-09-29 14:29:07 -0400

    asd

--- src/noop.clj
+++ src/noop.clj
@@ -1,6 +1,25 @@
-(ns noop)
+(ns noop
+  (:require [clojure.test :refer [function?]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn do-when-func
+  [arg x]
+  (when (function? x)
+    (x arg)))
+
+(defn noop-first
+  [& args]
+  (let [keep (first args)
+        _ (mapv #(do-when-func keep %) args)]
+    keep))
+
+(comment
+  
+  (noop-first 12 #(println (str "hello: " %)))
+  
+
+  
+
+
+
+
+  )

370c5708a47dd5c3762c439bf96e15d40b6389f9
gkspranger <greg@spranger.us>
2022-10-12 15:34:30 -0400

    some reuse

--- src/throwaway.clj
+++ src/throwaway.clj
@@ -1,19 +1,20 @@
 (ns throwaway)
 
+(defn- run-impl
+  [keep args]
+  (run! #(try (% keep) (catch Exception _)) args)
+  keep)
+
 (defn constantly-run-first!
   "Runs the supplied procedures (any argument that is not the first argument)
    for purposes of side effects, while always passing the first argument as a
    parameter to successive arguments. Returns the first argument."
   [& args]
-  (let [keep (first args)]
-    (run! #(% keep) (rest args))
-    keep))
+  (run-impl (first args) (rest args)))
 
 (defn constantly-run-last!
   "Runs the supplied procedures (any argument that is not the last argument)
    for purposes of side effects, while always passing the last argument as a
    parameter to preceding arguments. Returns the last argument."
   [& args]
-  (let [keep (last args)]
-    (run! #(% keep) (drop-last args))
-    keep))
+  (run-impl (last args) (drop-last args)))
